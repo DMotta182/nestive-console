98f129384cd298dab5907d9807d2fc1a963aec97
DMotta182 <motta2194@gmail.com>
2020-03-03 19:49:53 -0600

    Login button functionality added on 403

--- src/components/error403/error403.component.jsx
+++ src/components/error403/error403.component.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container'
+import { useHistory } from 'react-router-dom';
 
-const Error403 = () => (
+const Error403 = () => {
+    const history = useHistory();
+    return (
     <div>
         <Container>
             <h2>You are not authorized to enter this page</h2>
             <h3>Please Login to continue</h3>
             <h3>Error 403</h3>
-            <Button>
+            <Button onClick={ () => history.push('/login') }>
                 Login
             </Button>
         </Container>
     </div>
-)
+    )
+}
 
 export default Error403;
